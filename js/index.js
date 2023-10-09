e9d802bfbd246da1e38ec1f3b0b70b120f7aae63
Bekzat <developer.bekzat@gmail.com>
2023-10-09 17:21:54 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    }
+});
+
